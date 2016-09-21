4d7a493730d59e3362c432a2e81bc37715bfb1a5
Sanket Sabnis <sanket.v.sabnis@gmail.com>
2016-09-21 02:59:05 -0700

    Added Drophighest and Droplowest functionality to evaluation, better printing, sorting results based of EFF

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import math
 import random
+import re
 from numpy import mean
 
-def roll(die, randomize=False,fighting_style=None,crit=False,crit_chance=False):
+operators = ["+","-","*","/","(",")","."]
+def roll(die, randomize=False,fighting_style=None,crit=False,crit_chance=None):
     die_size = int(die.split("d")[-1])
     value = 0
     if randomize:
@@ -12,17 +14,44 @@ def roll(die, randomize=False,fighting_style=None,crit=False,crit_chance=False):
         value = (die_size-2)/float(die_size)*mean(range(3,die_size+1)) + 2/float(die_size)*roll(die, randomize)
     else:
         value = (die_size+1)/float(2)
-    if crit:
-        value += (0.05*roll(die, randomize, gwf))
+    if crit and crit_chance:
+        value += (crit_chance*roll(die, randomize, fighting_style, crit=False))
     if fighting_style=="DUELLING":
         value +=2
     return value
 
+def get_die(pos, tokens):
+    die = ""
+    multiplier = ""
+    if pos>0 and tokens[pos-1].isdigit():
+        multiplier = "*"
+    while(tokens[pos] not in operators):
+        die += tokens[pos]
+        if pos == len(tokens)-1:
+            break
+        pos += 1
+    return die, multiplier, pos
+
+def parse_match(m, droplowest=False):
+    if len(m.groups()) ==2:
+        num_dice = int(m.group(1))
+        die = m.group(2)
+        if die:
+            die_size = int(die.split("d")[-1])
+            sum_dies = (die_size +1)*(num_dice/2.0)*pow(die_size, num_dice)
+            drop_sum = 0
+            for die_face in range(1, die_size+1):
+                if droplowest:
+                    all_face = pow(die_size+1-die_face, num_dice) - pow(die_size-die_face, num_dice)
+                else:
+                    all_face = pow(die_face, num_dice) - pow(die_face-1, num_dice)
+                drop_sum += die_face*(all_face)
+            avgroll = (sum_dies - drop_sum)/float(pow(die_size, num_dice))
+    return avgroll
+
 def evaluate(string, randomize=False, fighting_style=None, crit=False,crit_chance=0.05):
     tokens = strip_spaces(string)
     eval_tokens = []
-    operators = ["+","-","*","/","(",")","."]
-    die = ""
     pos = 0
     while (pos <len(tokens)):
         token = tokens[pos]
@@ -33,23 +62,31 @@ def evaluate(string, randomize=False, fighting_style=None, crit=False,crit_chanc
         elif token in operators or token.isdigit():
             eval_tokens.append(token)
         elif "d" == token:
-            if pos>0 and tokens[pos-1].isdigit():
-                multiplier = "*"
-            while(tokens[pos] not in operators):
-                die += tokens[pos]
-                if pos == len(tokens)-1:
-                    break
-                pos += 1
+            die, multiplier, pos = get_die(pos, tokens)
             if die:
                 die_roll = roll(die, randomize, fighting_style, crit, crit_chance)
                 eval_tokens.append("%s%s"%(multiplier, die_roll))
-                multiplier = ""
             if pos != len(tokens)-1:
                 eval_tokens.append(tokens[pos])
-
-            die = ""
+        elif "D" == token:
+            # Search for highest , highest = drop lowest
+            patt = r"Droplowest(\d+)(d\d+)"
+            p = re.compile(patt)
+            m = re.search(p, tokens[pos:])
+            if m and m.groups():
+                avgroll = parse_match(m, droplowest=True)
+                if avgroll: eval_tokens.append(avgroll)
+                pos += len(m.group(0))
+            else:
+                # Search for lowest , lowest = drop highest
+                patt = r"Drophighest(\d+)(d\d+)"
+                p = re.compile(patt)
+                m = re.search(p, tokens[pos:])
+                if m and m.groups():
+                    avgroll = parse_match(m, droplowest=False)
+                    if avgroll: eval_tokens.append(avgroll)
+                    pos += len(m.group(0))
         pos += 1
-        #print eval_tokens
     eval_str = "".join(map(str,eval_tokens))
     return eval(eval_str)
 
@@ -57,6 +94,6 @@ def strip_spaces(string):
     tokens = []
     for ch in string:
         if ch == " ":
-            pass
+           continue
         tokens.append(ch)
     return "".join(tokens)
--- main.py
+++ main.py
@@ -7,33 +7,34 @@ from player import *
 
 if __name__ == "__main__":
     p1 = Character(name="Polearm + GWF",feats=["polearm_master"],fighting_style="GWF",default=1)
-    p2 = Character(name="Sword and Board",fighting_style="DUELLING",weapon="d8",consitution=16,default=1)
-    p3 = Character(name="Polearm + Defense",feats=["polearm_master"],fighting_style="DEFENSE",default=1)
-    p4 = Character(name="GreatSword + GWF",fighting_style="GWF",weapon="2d6",default=1)
-    p5 = Character(name="GreatSword + Defense",fighting_style="DEFENSE",weapon="2d6",default=1)
-    p6 = Character(name="Polearm + Duelling",feats=["polearm_master"],fighting_style="DUELLING",weapon="d6",default=1)
-    p7 = Character(name="Great Axe",fighting_style="GWF",weapon="d12",default=1)
-    p8 = Character(name="TANK",fighting_style="DUELLING",weapon="d8",default=1,ac=18,feats=["ham"])
+    p2 = Character(name="Polearm + Defense",feats=["polearm_master"],fighting_style="DEFENSE",default=1)
+    p3 = Character(name="GreatSword + GWF",fighting_style="GWF",weapon="2d6",default=1)
+    p4 = Character(name="GreatSword + Defense",fighting_style="DEFENSE",weapon="2d6",default=1)
+    p5 = Character(name="Polearm + Duelling",feats=["polearm_master"],fighting_style="DUELLING",weapon="d6",default=1)
+    p6 = Character(name="Great Axe + GWF",fighting_style="GWF",weapon="d12",default=1)
+    p7 = Character(name="Sword + Shield",fighting_style="DUELLING",weapon="d8",consitution=16,default=1)
+    p8 = Character(name="Sword + Shield + HAM",fighting_style="DUELLING",weapon="d8",default=1,feats=["ham"])
+    p9 = Character(name="Polearm + HAM + Defense",fighting_style="DEFENSE",weapon="d10",default=1,feats=["ham", "polearm_master"], strength=16)
     m = Character(monster=1)
-    players = [p1, p2, p3, p4, p5, p6, p7, p8]
-    stats = ["name", "dpr", "dtpr", "ris", "eff", "dmg", "odmg"]
+    players = [p1, p2, p3, p4, p5, p6, p7, p8, p9]
+    stats = ["name", "ac", "weapon", "dpr", "dtpr", "ris", "eff", "dmg", "odmg"]
     headers = {}
     player_stats = []
     g_tokens = []
     for p in players:
         p_stats = {}
         for stat in stats:
-            if stat == "name":
-                val = "%s"%(p.name)
+            if stat in ["name", "ac", "weapon"]:
+                val = "%s"%(getattr(p, stat))
             else:
                 val = "%.3f"% (getattr(p, stat)(m))
             p_stats[stat] = val
-            headers[stat] = max(headers.get(stat,0), len(val))
+            headers[stat] = max(headers.get(stat,0), len(val), len(stat))
         player_stats.append(p_stats)  
     for stat in stats:
         g_tokens.append("%s%s  " % (stat.upper()," "*(headers[stat] - len(stat))))
     g_tokens.append("\n%s%s"% ("-"*(sum(len(token) for token in g_tokens)),"\n"))
-    for p_stat in player_stats:
+    for p_stat in sorted(player_stats, key=lambda x: float(x.get("eff")), reverse=True):
         for stat in stats:
             g_tokens.append("%s%s  " % (p_stat[stat]," "*(headers[stat] - len(p_stat[stat]))))
         g_tokens.append("\n")
