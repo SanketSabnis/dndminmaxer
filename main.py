e5e54f28a424298965944f496348ac30b1ab4392
Sanket Sabnis <sanket@Sankets-MacBook-Pro-2.local>
2016-09-20 22:57:32 -0700

    Updated gitignore and improved printing format

--- main.py
+++ main.py
@@ -11,28 +11,32 @@ if __name__ == "__main__":
     p3 = Character(name="Polearm + Defense",feats=["polearm_master"],fighting_style="DEFENSE",default=1)
     p4 = Character(name="GreatSword + GWF",fighting_style="GWF",weapon="2d6",default=1)
     p5 = Character(name="GreatSword + Defense",fighting_style="DEFENSE",weapon="2d6",default=1)
-    p6 = Character(name="Polearm + Duelling (Cheese)",feats=["polearm_master"],fighting_style="DUELLING",weapon="d6",default=1)
+    p6 = Character(name="Polearm + Duelling",feats=["polearm_master"],fighting_style="DUELLING",weapon="d6",default=1)
     p7 = Character(name="Great Axe",fighting_style="GWF",weapon="d12",default=1)
     p8 = Character(name="TANK",fighting_style="DUELLING",weapon="d8",default=1,ac=18,feats=["ham"])
-    p9 = Character(name="Cleric",consitution=19,weapon="d12",attacks=1,strength=17,default=1)
     m = Character(monster=1)
-    players = [p1, p2, p3, p4, p5, p6, p7, p8, p9]
-    stats = ["name", "dpr ", "dtpr", "ris ", "eff ", "dmg ", "odmg"]
-    len_max_name = max(len(p.name) for p in players)
-    headers = []
-    for x in stats:
-        headers.append("%s%s" % (x.upper(),(" "*(len_max_name - len("name") +5) if x =="name" else "\t ")))
-    header_str= "".join(headers)
-    print "%s\n%s"% (header_str, "-"*(int(1.25*len(header_str))))
+    players = [p1, p2, p3, p4, p5, p6, p7, p8]
+    stats = ["name", "dpr", "dtpr", "ris", "eff", "dmg", "odmg"]
+    headers = {}
+    player_stats = []
+    g_tokens = []
     for p in players:
-        num_spaces = (len_max_name - len(p.name))+5
-        tokens = []
+        p_stats = {}
         for stat in stats:
             if stat == "name":
-                val = p.name
-                tokens.append("%s%s"%(val," "*num_spaces))
+                val = "%s"%(p.name)
             else:
-                val = getattr(p, stat.strip())(m)
-                tokens.append("%.3f\t "% (val))
-        print "".join(tokens)
+                val = "%.3f"% (getattr(p, stat)(m))
+            p_stats[stat] = val
+            headers[stat] = max(headers.get(stat,0), len(val))
+        player_stats.append(p_stats)  
+    for stat in stats:
+        g_tokens.append("%s%s  " % (stat.upper()," "*(headers[stat] - len(stat))))
+    g_tokens.append("\n%s%s"% ("-"*(sum(len(token) for token in g_tokens)),"\n"))
+    for p_stat in player_stats:
+        for stat in stats:
+            g_tokens.append("%s%s  " % (p_stat[stat]," "*(headers[stat] - len(p_stat[stat]))))
+        g_tokens.append("\n")
+    print "".join(g_tokens)
+
 
